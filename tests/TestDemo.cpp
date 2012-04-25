ad41427d1e7b1b51f83ec459b57ce5c1dc6ed5a6
Ken Dwyer <dwyer@cs.ualberta.ca>
2012-04-25 15:01:13 -0600

    Added a non-trivial unit test.

--- tests/TestDemo.cpp
+++ tests/TestDemo.cpp
@@ -1,12 +1,109 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE TestDemo
+
+#include "Alphabet.h"
+#include "BiasFeatureGen.h"
+#include "BmrmOptimizer.h"
+#include "Dataset.h"
+#include "EmptyAlignmentFeatureGen.h"
+#include "FeatureVector.h"
+#include "KlementievRothWordFeatureGen.h"
+#include "LbfgsOptimizer.h"
+#include "LogFeatArc.h"
+#include "LogLinearBinary.h"
+#include "LogLinearBinaryObs.h"
+#include "LogLinearMulti.h"
+#include "Optimizer.h"
+#include "RealWeight.h"
+#include "StringEditModel.h"
+#include "Utility.h"
+#include "WeightVector.h"
+#include "WordAlignmentFeatureGen.h"
+#include "WordPairReader.h"
+#include <boost/test/floating_point_comparison.hpp>
 #include <boost/test/unit_test.hpp>
 
-#include "WeightVector.h"
- 
-BOOST_AUTO_TEST_CASE(universeInOrder)
-{
-  WeightVector w;
-  BOOST_CHECK(w.getDim() == 0);
-}
 
+BOOST_AUTO_TEST_CASE(testLogLinearMulti)
+{
+  const int argc = 9;
+  char* argv[argc];
+  argv[0] = (char*) "latent_struct";
+  argv[1] = (char*) "--order=0";
+  argv[2] = (char*) "--no-align-ngrams";
+  argv[3] = (char*) "--no-collapsed-align-ngrams";
+  argv[4] = (char*) "--restarts=2";
+  argv[5] = (char*) "--quiet";
+  argv[6] = (char*) "--no-normalize";
+  argv[7] = (char*) "--bias-no-normalize";
+  argv[8] = (char*) "--no-final-arc-feats";
+  
+  shared_ptr<Alphabet> alphabet(new Alphabet(false, false));
+  shared_ptr<BiasFeatureGen> fgenObs(new BiasFeatureGen(alphabet));
+  int ret = fgenObs->processOptions(argc, argv);
+  BOOST_CHECK_EQUAL(ret, 0);
+  shared_ptr<WordAlignmentFeatureGen> fgenLat(new WordAlignmentFeatureGen(
+      alphabet));
+  ret = fgenLat->processOptions(argc, argv);
+  BOOST_CHECK_EQUAL(ret, 0);
+  Model* model = new StringEditModel<LogFeatArc>(fgenLat, fgenObs);
+  ret = model->processOptions(argc, argv);
+  BOOST_CHECK_EQUAL(ret, 0);
+  
+  vector<Model*> models;
+  models.push_back(model);  
+  
+  Dataset trainData;
+  WordPairReader reader;
+  bool badFile = Utility::loadDataset(reader, "he_tiny", trainData);
+  BOOST_REQUIRE(!badFile);
+  
+  LogLinearMulti objective(trainData, models);
+  size_t maxNumFvs = 0, totalNumFvs = 0;
+  objective.gatherFeatures(maxNumFvs, totalNumFvs);
+  BOOST_REQUIRE(maxNumFvs > 0 && totalNumFvs > 0);
+  
+  BOOST_CHECK(!alphabet->isLocked());
+  alphabet->lock();
+  const int d = alphabet->size();
+  BOOST_REQUIRE_EQUAL(d, 8);
+  
+  WeightVector W(d);
+  
+  // set the feature weight for bias class y=0 to one
+  int index = alphabet->lookup("0_Bias", false);
+  BOOST_REQUIRE(index >= 0);
+  W.add(index, 1.0);
+  BOOST_REQUIRE_EQUAL(W.getWeight(index), 1.0);
+  
+  FeatureVector<RealWeight> gradFv(d);
+  double fval;
+  objective.valueAndGradient(W, fval, gradFv);
+  BOOST_CHECK_CLOSE(0.81326168751, fval, 1e-8);
+  BOOST_CHECK_CLOSE(0.23105857863, gradFv.getValueAtLocation(0).value(), 1e-8);
+  BOOST_CHECK_CLOSE(0.71278741450, gradFv.getValueAtLocation(1).value(), 1e-8);
+  BOOST_CHECK_CLOSE(0.69725812519, gradFv.getValueAtLocation(2).value(), 1e-8);
+  BOOST_CHECK_CLOSE(0.30803476795, gradFv.getValueAtLocation(3).value(), 1e-8);
+  BOOST_CHECK_CLOSE(-gradFv.getValueAtLocation(0).value(),
+      gradFv.getValueAtLocation(4).value(), 1e-8);
+  BOOST_CHECK_CLOSE(-gradFv.getValueAtLocation(1).value(),
+      gradFv.getValueAtLocation(5).value(), 1e-8);
+  BOOST_CHECK_CLOSE(-gradFv.getValueAtLocation(2).value(),
+      gradFv.getValueAtLocation(6).value(), 1e-8);
+  BOOST_CHECK_CLOSE(-gradFv.getValueAtLocation(3).value(),
+      gradFv.getValueAtLocation(7).value(), 1e-8);
+  
+  LbfgsOptimizer opt(objective);
+  ret = opt.processOptions(argc, argv);
+  BOOST_REQUIRE_EQUAL(0, ret);
+  opt.setBeta(0.1);
+  BOOST_REQUIRE_EQUAL(opt.getBeta(), 0.1);
+  
+  double fvalOpt = 0.0;
+  Optimizer::status status = opt.train(W, fvalOpt, 1e-5);
+  BOOST_REQUIRE(status == Optimizer::CONVERGED);
+  
+  objective.valueAndGradient(W, fval, gradFv);
+  Utility::addRegularizationL2(W, opt.getBeta(), fval, gradFv);
+  BOOST_CHECK_CLOSE(0.67414588974, fval, 1e-8);
+}
