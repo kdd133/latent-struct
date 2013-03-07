4a96986e2abafb27cde17d5bf6e965e9f7cf8d06
Ken Dwyer <dwyer@cs.ualberta.ca>
2013-03-07 11:34:56 -0700

    RegularizerSoftTying now permits beta values to equal zero.

--- src/optimization/RegularizerSoftTying.cpp
+++ src/optimization/RegularizerSoftTying.cpp
@@ -63,7 +63,7 @@ int RegularizerSoftTying::processOptions(int argc, char** argv) {
 
 void RegularizerSoftTying::addRegularization(const Parameters& theta,
     double& fval, RealVec& grad) const {
-  assert(_betaW > 0 && _betaU > 0 && _betaSharedW > 0 && _betaSharedU > 0);
+  assert(_betaW >= 0 && _betaU >= 0 && _betaSharedW >= 0 && _betaSharedU >= 0);
   assert(_labelShared > 0);
   assert(_alphabet && _labels);
   assert(theta.getTotalDim() == grad.size());
