aa2ed137242e4c23456257bfa9bc5544603e0675
Ken Dwyer <dwyer@cs.ualberta.ca>
2013-07-18 18:08:15 -0600

    Added an option to extract_freq_ngrams.py to base cutoff on probability mass.

--- cognate_phrases/extract_freq_ngrams.py
+++ cognate_phrases/extract_freq_ngrams.py
@@ -20,6 +20,8 @@ def main():
                       help='write the k most frequent n-grams from each file')
     parser.add_option('-o', '--output', type='string', default=None,
                       help='name of the output file (default: None)')
+    parser.add_option('-p', '--prob-mass', type='float', default=None,
+                      help='write the n-grams that account for the given probability mass in each file')
     opts, args = parser.parse_args()
     
     if len(args) < 1:
@@ -32,26 +34,33 @@ def main():
         parser.print_help()
         return
     
-    if not (opts.cut_fraction or opts.top_k):
-        print('Error: Either --cut-fraction or --top-k must be specified.')
+    if not (opts.cut_fraction or opts.top_k or opts.prob_mass):
+        print('Error: Either --cut-fraction or --prob-mass or --top-k must be specified.')
         parser.print_help()
         return
     
     if opts.top_k:
         print('using --top-k=%d' % opts.top_k)
-    else:
+        assert opts.top_k > 0
+    elif opts.cut_fraction:
         print('using --cut-fraction=%g' % opts.cut_fraction)
+        assert 0.0 < opts.cut_fraction < 1.0
+    else:
+        print('using --prob-mass=%g' % opts.prob_mass)
+        assert 0.0 < opts.prob_mass < 1.0
     
     fout = codecs.open(opts.output, 'w', encoding='latin1')        
     for fname in args:
         f = codecs.open(fname, 'r', encoding='latin1')
         if opts.top_k:
             writeMostFrequentNgrams(f, fout, opts.top_k)
-        else:
+        elif opts.cut_fraction:
             counts, cumul = getCumulativeCounts(f)
             min_count = getCountForFraction(counts, cumul, opts.cut_fraction)
             print('min_count for %s is %d' % (fname, min_count))
             writeNgramsWithMinCount(f, fout, min_count)
+        else:
+            writeNgramsWithTotalMass(f, fout, opts.prob_mass)
         f.close()
     fout.close()
 
@@ -64,7 +73,7 @@ def writeNgramsWithMinCount(fin, fout, min_count):
         count = int(line.split()[-1])
         if count >= min_count:
             fout.write(raw_line)
-            
+
 def writeMostFrequentNgrams(fin, fout, k):
     fin.seek(0) # rewind
     freq = []
@@ -78,6 +87,28 @@ def writeMostFrequentNgrams(fin, fout, k):
     freq.sort(reverse=True)
     for count, raw_line in freq[:k]:
         fout.write(raw_line)
+
+def writeNgramsWithTotalMass(fin, fout, mass_desired):
+    fin.seek(0) # rewind
+    freq = []
+    total_count = 0
+    for raw_line in fin.readlines():
+        line = raw_line.rstrip()
+        if len(line) == 0:
+            continue
+        count = int(line.split()[-1])
+        total_count += count
+        freq.append((count, raw_line))        
+    print('Total n-gram count is: %d' % total_count)
+    freq.sort(reverse=True)    
+    mass_consumed = 0.0
+    for c, raw_line in freq:
+        p = float(c) / total_count
+        if mass_consumed + p > mass_desired:
+            break
+        mass_consumed += p
+        fout.write(raw_line)
+    print('Mass consumed is %g' % mass_consumed)
 
 if __name__ == "__main__":
     main()
--- cognate_phrases/master.sh
+++ cognate_phrases/master.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 f=${1}
+nghi=${2}
 
 nglo=2
-nghi=4
 cut=0.95
 k=250
+p=0.10
 #method="cut"
+#method="top"
 
-if [ "${f}" == "" ]; then
-  echo "Usage: master.sh <cognate .train file>"
+if [ "${f}" == "" ] || [ "${nghi}" == "" ]; then
+  echo "Usage: master.sh <cognate .train file> <max n-gram size>"
   exit 1
 fi
 
@@ -30,13 +32,17 @@ echo "./generate_ngrams.sh ${src} ${nglo} ${nghi}"
 echo "./generate_ngrams.sh ${tgt} ${nglo} ${nghi}"
 ./generate_ngrams.sh ${tgt} ${nglo} ${nghi}
 
-
+ext=${nghi}glex
 if [ "${method}" == "cut" ]; then
   # Create n-gram lexicons based on highest percentage of n-grams.
-  python2 extract_freq_ngrams.py -c ${cut} -o ${src}.nglex ${src}.*grams
-  python2 extract_freq_ngrams.py -c ${cut} -o ${tgt}.nglex ${tgt}.*grams
-else
+  python2 extract_freq_ngrams.py -c ${cut} -o ${src}.c${cut}.${ext} ${src}.*grams
+  python2 extract_freq_ngrams.py -c ${cut} -o ${tgt}.c${cut}.${ext} ${tgt}.*grams
+elif [ "${method}" == "top" ]; then
   # Create n-gram lexicons based on k most frequent n-grams.
-  python2 extract_freq_ngrams.py -k ${k} -o ${src}.nglex ${src}.*grams
-  python2 extract_freq_ngrams.py -k ${k} -o ${tgt}.nglex ${tgt}.*grams
+  python2 extract_freq_ngrams.py -k ${k} -o ${src}.k${k}.${ext} ${src}.*grams
+  python2 extract_freq_ngrams.py -k ${k} -o ${tgt}.k${k}.${ext} ${tgt}.*grams
+else
+  # Create n-gram lexicons that make up the given amount of probability mass.
+  python2 extract_freq_ngrams.py -p ${p} -o ${src}.p${p}.${ext} ${src}.*grams
+  python2 extract_freq_ngrams.py -p ${p} -o ${tgt}.p${p}.${ext} ${tgt}.*grams
 fi
